36aaedfe4d3870e5c2cf45a67c5076db832f0147
srividhiyaJ <118594248+srividhiyaJ@users.noreply.github.com>
2022-11-20 12:01:17 +0000

    Adding Authentication header files.

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WishList.Models;
 namespace WishList.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
